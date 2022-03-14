070f4d93bc4074bb9d38a7772abb87ccaa8b854b
Shreyas Gangurde <57534229+shreyasg-git@users.noreply.github.com>
2022-03-14 14:56:59 +0530

    WT 01 done

--- All Sorts Of Things/BubbleSort.java
+++ All Sorts Of Things/BubbleSort.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-class SelectionSort {
-    // Declaring ANSI_RESET so that we can reset the color
+public class BubbleSort {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
     public static final String ANSI_YELLOW = "\u001B[33m";
@@ -13,30 +12,21 @@ class SelectionSort {
     static int[] arr = { 2, 5, 7, 3, 4, 9, 5 };
 
     public static void main(String[] args) {
-        if (args[0].equals("true")) {
-            takeCustomInputsFlag = true;
-        } else {
-            takeCustomInputsFlag = false;
-        }
-        if (takeCustomInputsFlag) {
-            takeInputs();
-            execSort();
-        } else {
-            execSort();
-        }
+        execBubble();
     }
 
-    public static void execSort() {
+    private static void execBubble() {
+        int counter = 1;
         for (int i = 0; i < arr.length; i++) {
-            int smallest = arr[i];
-            int smallestIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < smallest) {
-                    smallest = arr[j];
-                    smallestIndex = j;
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1);
+                } else {
+                    printArr(arr);
                 }
+                System.out.println(counter);
+                counter++;
             }
-            swap(i, smallestIndex);
         }
     }
 
@@ -45,17 +35,6 @@ class SelectionSort {
         arr[src] = arr[target];
         arr[target] = temp;
         printArrAndHighlight(arr, new Integer[] { src, target });
-    }
-
-    public static void takeInputs() {
-        System.out.print("Enter the length of given array : ");
-        lenOfArr = in.nextInt();
-        arr = new int[lenOfArr];
-        System.out.println("Please enter the numbers one by one followed by Enter key:");
-        for (int i = 0; i < lenOfArr; i++) {
-            System.out.print("Enter element at position - " + i + ": ");
-            arr[i] = in.nextInt();
-        }
     }
 
     public static void printArrAndHighlight(int[] arr, Integer[] highlightIndices) {
@@ -78,5 +57,4 @@ class SelectionSort {
         }
         System.out.println("]");
     }
-
 }
--- Lp2/02/KruskalsMST.java
+++ Lp2/02/KruskalsMST.java
@@ -1,22 +1,44 @@
+import java.util.*;
+
+// Sort all the edges by weight
+// start from leawst weight
 class KruskalMST {
     static Scanner in = new Scanner(System.in);
     static int numOfNodes;
     static int numOfEdges;
     static int dest;
     static int[][] matrix = {
-            /*0*/{ 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-            /*1*/{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-            /*2*/{ 0, 8, 0, 7, 0, 4, 0, 0, 2},
-            /*3*/{ 0, 0, 7, 0, 3, 14, 0, 0, 0 },
-            /*4*/{ 0, 0, 0, 3, 0, 10, 0, 0, 0 },
-            /*5*/{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-            /*6*/{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-            /*7*/{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-            /*8*/{ 0, 0, 2, 0, 0, 0, 6, 7, 0 };
+            /* 0 */{ 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+            /* 1 */{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+            /* 2 */{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+            /* 3 */{ 0, 0, 7, 0, 3, 14, 0, 0, 0 },
+            /* 4 */{ 0, 0, 0, 3, 0, 10, 0, 0, 0 },
+            /* 5 */{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+            /* 6 */{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+            /* 7 */{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+            /* 8 */{ 0, 0, 2, 0, 0, 0, 6, 7, 0 }
     };
+
+    static int[][] ansMST = new int[9][9];
+
+    // DFS implementation for cycle detection
 
     public static void main(String[] args) {
 
     }
 
+    public static void checkForCycle(int[][] tree, int node1, int node2) {
+        boolean[] visited = new boolean[tree.length];
+        for (int i = 0; i < visited.length; i++) {
+            visited[i] = false;
+        }
+
+        boolean isConnectedAlready = checkUsingDFS(visited);
+
+    }
+
+    private static boolean checkUsingDFS(boolean[] vis) {
+        
+        vis[]
+    }
 }
